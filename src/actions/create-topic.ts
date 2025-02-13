42e65f24277d7c9a1be24d38aa545a6a5502b3dc
Debanjan <debanjanghosal10@gmail.com>
2025-02-13 12:09:14 +0530

    use zod in server action to perform server-side form validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,38 @@
 "use server";
-export const createTopicAction = async () => {
+import { z } from "zod";
+
+// creating schema for validation
+const topicSchema = z.object({
+  title: z
+    .string()
+    .min(5, "Title must be at least 5 characters long.")
+    .max(100, "Title cannot exceed 100 characters.")
+    .regex(
+      /^[a-zA-Z0-9\s.,!?-]+$/,
+      "Title can only contain letters, numbers, spaces, and basic punctuation."
+    ),
+  description: z
+    .string()
+    .min(10, "Description must be at least 10 characters long.")
+    .max(1000, "Description cannot exceed 1000 characters."),
+});
+
+export const createTopicAction = async (formData: FormData) => {
+  // getting hold of data that is to be validated
+  const title = formData.get("title");
+  const description = formData.get("description");
+
+  // perform validation checks
+  const result = topicSchema.safeParse({ title, description });
+  if (result.success) {
+    console.log("Validation successful!", result.data);
+  } else {
+    console.log("Validation Failed");
+    console.log(result.error.flatten().fieldErrors);
+  }
+
   // revalidate home path
+
+  // redirect user to that topic route
   return;
 };
--- src/components/topics/CreateTopicModal.tsx
+++ src/components/topics/CreateTopicModal.tsx
@@ -13,49 +13,54 @@ import {
 } from "@heroui/react";
 
 import React from "react";
+import * as actions from "@/actions";
 
 const CreateTopicModal = () => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <form className="my-4">
+    <div className="my-4">
       <Button onPress={onOpen} color="secondary">
         New Topic
       </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="text-2xl flex flex-col gap-1">
-                Create A Topic
-              </ModalHeader>
-              <ModalBody>
-                <Input
-                  className="w-full"
-                  color="primary"
-                  label="Title"
-                  type="text"
-                />
-                <Textarea
-                  className="w-full"
-                  color="primary"
-                  defaultValue=""
-                  label="Description"
-                  type="text"
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button type="submit" color="primary" onPress={onClose}>
-                  Submit
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
+        <form action={actions.createTopicAction}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="text-2xl flex flex-col gap-1">
+                  Create A Topic
+                </ModalHeader>
+                <ModalBody>
+                  <Input
+                    name="title"
+                    className="w-full"
+                    color="primary"
+                    label="Title"
+                    type="text"
+                  />
+                  <Textarea
+                    name="description"
+                    className="w-full"
+                    color="primary"
+                    defaultValue=""
+                    label="Description"
+                    type="text"
+                  />
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                  </Button>
+                  <Button type="submit" color="primary" onPress={onClose}>
+                    Submit
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </form>
       </Modal>
-    </form>
+    </div>
   );
 };
 
